3c60d6807676c11ca1b696f30fa4dbd6687694e4
StasTikh <tmihatelefan@gmail.com>
2021-10-10 21:10:31 +0300

    final version

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,86 +7,39 @@ function deCoder() {
                  '6', '7', '8', '9'],
         meaNing: ['Alpha', 'Bravo', 'Charlie', 'Delta', 
                   'Echo', 'Foxtrot', 'Golf', 'Hotel',
-                  'Kilo', 'Lima', 'Mike', 'November',
-                  'Oscar', 'Papa', 'Quebec', 'Romeo',
-                  'Sierra', 'Tango', 'Uniform', 'Victor',
-                  'Whiskey', 'X-Ray', 'Yankee', 'Zulu',
-                  'Zero', 'One', 'Two', 'Three', 'Four',
-                  'Five', 'Six', 'Seven', 'Eight', 'Nine' 
-                ]
+                  'India', 'Juliett', 'Kilo', 'Lima',
+                  'Mike', 'November', 'Oscar', 'Papa',
+                  'Quebec', 'Romeo', 'Sierra', 'Tango',
+                  'Uniform', 'Victor', 'Whiskey', 'X-Ray',
+                  'Yankee', 'Zulu', 'Zero', 'One', 'Two',
+                  'Three', 'Four', 'Five', 'Six', 'Seven',
+                  'Eight', 'Nine']
     }
         
-    
-
-
     let codedPhrase = inputCode.value;
     let finalPhrase = [];
-
-
-    if (codedPhrase == codedPhrase.toUpperCase()) {
-        codedPhrase = codedPhrase.toLowerCase();    
-    }
-    //console.log(codedPhrase);
-
     let codeCheck = codedPhrase.split('');
-
-    console.log(codeCheck);
-
-    for(i=0; i<vocab.symBol.length; i++){
-        for(j=0; j<codeCheck.length; j++){
-            if(codeCheck[j] == vocab.symBol[i]){
-                finalPhrase.push(vocab.meaNing[i-2])
-                console.log(finalPhrase);
+    
+    
+    for(i=0; i<codeCheck.length; i++){
+        if (codeCheck[i] == codeCheck[i].toUpperCase()) {
+            codeCheck[i] = codeCheck[i].toLowerCase();
+            console.log(codeCheck);       
+        }
+    }
+    
+    for(i=0; i<codeCheck.length; i++){
+        for(k=0; k<vocab.meaNing.length; k++) {
+            if(codeCheck[i] === vocab.symBol[k]) {
+                finalPhrase.push(vocab.meaNing[k])
             }
         }
     }
 
-
-
-console.log(finalPhrase);
-//outputCode.innerHTML = `The frase is ${finalPhrase}`;
+    outputCode.innerHTML = `The frase is ${finalPhrase}`;
 
 }
 
 
 
 
-/*
-{symBol: 'a', meaning: 'Alpha'},
-        {symBol: 'b', meaning: 'Bravo'},
-        {symBol: 'c', meaning: 'Charlie'},
-        {symBol: 'd', meaning: 'Delta'},
-        {symBol: 'e', meaning: 'Echo'},
-        {symBol: 'f', meaning: 'Foxtrot'},
-        {symBol: 'g', meaning: 'Golf'},
-        {symBol: 'h', meaning: 'Hotel'},
-        {symBol: 'i', meaning: 'India'},
-        {symBol: 'j', meaning: 'Juliett'},
-        {symBol: 'k', meaning: 'Kilo'},
-        {symBol: 'l', meaning: 'Lima'},
-        {symBol: 'm', meaning: 'Mike'},
-        {symBol: 'n', meaning: 'November'},
-        {symBol: 'o', meaning: 'Oscar'},
-        {symBol: 'p', meaning: 'Papa'},
-        {symBol: 'q', meaning: 'Quebec'},
-        {symBol: 'r', meaning: 'Romeo'},
-        {symBol: 's', meaning: 'Sierra'},
-        {symBol: 't', meaning: 'Tango'},
-        {symBol: 'u', meaning: 'Uniform'},
-        {symBol: 'v', meaning: 'Victor'},
-        {symBol: 'w', meaning: 'Whiskey'},
-        {symBol: 'x', meaning: 'X-ray'},
-        {symBol: 'y', meaning: 'Yankee'},
-        {symBol: 'z', meaning: 'Zulu'},
-        {symBol: '0', meaning: 'Zero'},
-        {symBol: '1', meaning: 'One'},
-        {symBol: '2', meaning: 'Two'},
-        {symBol: '3', meaning: 'Three'},
-        {symBol: '4', meaning: 'Four'},
-        {symBol: '5', meaning: 'Five'},
-        {symBol: '6', meaning: 'Six'},
-        {symBol: '7', meaning: 'Seven'},
-        {symBol: '8', meaning: 'Eight'},
-        {symBol: '9', meaning: 'Nine'},
-        {symBol: '100', meaning: 'hundred'}
-        */
